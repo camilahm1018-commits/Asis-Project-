c4f1ad48d550df54656952603da9e1714c5ee247
Alejo <alejito1568@gmail.com>
2026-09-03 16:10:49 -0500

    agregacion en el main de la nueva ruta cambio_estado

--- Backend/main.py
+++ Backend/main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
 from conexion_db import lifespan
-from enrutadores import roles, asig_tec, ticket, estados_ticket, His_tickets, Usuarios,ER_ambientes,ER_equipos,motivo_novedad,notificaciones,tipo_equipo,tipo_documento,dashboard,auth, cambio_estado
+from enrutadores import roles, asig_tec, ticket, estados_ticket, His_tickets, Usuarios,ER_ambientes,ER_equipos,motivo_novedad,notificaciones,tipo_equipo,tipo_documento,dashboard,auth,cambio_estado
+from fastapi.middleware.cors import CORSMiddleware
 
 asis = FastAPI(lifespan=lifespan, title="ASIS API")
 
 @asis.get("/")
 def prueba():
     return {"mensaje": "Servidor ASIS conectado correctamente"}
+
+
+# 2. ¡La magia del CORS! Le damos permiso a React para hablar con nosotros
+asis.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"], # El puerto por defecto de React (Vite)
+    allow_credentials=True,
+    allow_methods=["*"], # Permitimos todos los métodos (GET, POST, etc.)
+    allow_headers=["*"], # Permitimos todos los headers
+)
 
 # Registrar todos los routers
 asis.include_router(roles.asis)
